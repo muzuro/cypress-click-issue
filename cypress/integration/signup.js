13d89d91630062d83a282e3f509dd0f70b51230a
Zufar Muhamadeev <zufarm@gmail.com>
2018-09-09 20:29:06 +0300

    use click instead trigger

--- cypress/integration/signup.js
+++ cypress/integration/signup.js
@@ -2,7 +2,7 @@ describe('Signup test', function() {
     it('Test signup flow', function() {
         cy.visit('https://qurasense.com')
         cy.contains('Sign up for participation').click()
-        cy.get('.btn-yes > material-ripple').trigger('click')
+        cy.get('.btn-yes > material-ripple').click()
         cy.get('[ngcontrol=firstName] input').should('be.visible')
     })
 })
